d7f948494fad72e11beedbcca4da45b2abc53e3f
elitalpa <elitalpa@gmail.com>
2023-06-17 14:59:35 +0200

    terre02.py - done with show arguments in lines

---
+++ terre02.py
@@ -0,0 +1,19 @@
+##### Terre - Afficheur d’arguments #####
+# Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+
+import sys
+
+### Function ###
+def show_arguments_in_lines():
+    for each_argument in arguments:
+        print(each_argument)
+
+### Error ###
+
+### Parsing ###
+arguments = sys.argv[1:]
+
+### Problem Solving ###
+
+### Result ###
+show_arguments_in_lines()
