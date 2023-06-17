df46ead751bdc74a2dc48878a6a82042778b7213
elitalpa <elitalpa@gmail.com>
2023-06-17 14:57:16 +0200

    terre01.py - Program name exercise done

---
+++ terre01.py
@@ -0,0 +1,19 @@
+##### Terre - Nom du programme #####
+# Créez un programme qui affiche son nom de fichier.
+
+import sys
+
+### Function ###
+def get_filename():
+    filename_result = sys.argv[0]
+    return filename_result
+
+### Error ###
+
+### Parsing ###
+
+### Problem Solving ###
+filename = get_filename()
+
+### Result ###
+print(filename)
