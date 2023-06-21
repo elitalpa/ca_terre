583bf4779c004576201207b84fd0afdbb0dda70a
elitalpa <elitalpa@gmail.com>
2023-06-21 22:21:18 +0200

    terre12.py - adding custom error messages and improvements

--- terre12.py
+++ terre12.py
@@ -8,45 +8,48 @@ import sys
 def transform_24_to_12(time):
     time_result = ""
     hour_str = time[:2]
-    hour = int(time[:2])
+    hour_int = int(time[:2])
     minutes = time[3:5]
     time_indicator = time[-2:]
 
-    if time_indicator == "AM" and hour >= 1 and hour <= 11:
+    if time_indicator == "AM" and hour_int >= 1 and hour_int <= 11:
         return f"{hour_str}:{minutes}"
 
-    if time_indicator == "AM" and hour == 12:
+    if time_indicator == "AM" and hour_int == 12:
         return f"00:{minutes}"
 
-    if time_indicator == "PM" and hour >= 1 and hour <= 11:
-        return f"{hour + 12}:{minutes}"
+    if time_indicator == "PM" and hour_int >= 1 and hour_int <= 11:
+        return f"{hour_int + 12}:{minutes}"
 
-    if time_indicator == "PM" and hour == 12:
+    if time_indicator == "PM" and hour_int == 12:
         return f"12:{minutes}"
-
-def display_error_message_and_exit():
-    print("erreur.")
-    exit()
 
 def handle_argument_errors():
     if len(sys.argv) != 2:
-        display_error_message_and_exit()
+        print("erreur: Veuillez n'entrer qu'un (seul) argument.")
+        exit()
     if len(sys.argv[1]) != 7:
-        display_error_message_and_exit()
+        print("erreur: Veuillez ne pas entrer plus ou moins de 7 caractères.")
+        exit()
 
     if sys.argv[1][2] != ":":
-        display_error_message_and_exit()
+        print("erreur: Veuillez entrer l'heure au format hh:mmXM (Exemple: 01:30PM). Il manque le signe ':' au milieu.")
+        exit()
 
     if not sys.argv[1][:2].isdigit() or not sys.argv[1][3:5].isdigit():
-        display_error_message_and_exit()
+        print("erreur: Veuillez entrer l'heure au format hh:mmXM (Exemple: 01:30PM). Les heures et les minutes doivent être des nombres.")
+        exit()
 
     if int(sys.argv[1][:2]) < 1 or int(sys.argv[1][:2]) > 12:
-        display_error_message_and_exit()
-    if int(sys.argv[1][3:5]) > 60:
-        display_error_message_and_exit()
+        print("erreur: Veuillez entrer l'heure au format hh:mmXM (Exemple: 01:30PM). Le nombre d'heures ne peut être que de 01 à 12 compris.")
+        exit()
+    if int(sys.argv[1][3:5]) < 0 or int(sys.argv[1][3:5]) > 60:
+        print("erreur: Veuillez entrer l'heure au format hh:mmXM (Exemple: 01:30PM). Le nombre de minutes ne peut être que de 00 à 60 compris.")
+        exit()
 
     if sys.argv[1][5:7] != "AM" and sys.argv[1][5:7] != "PM":
-        display_error_message_and_exit()
+        print("erreur: Veuillez entrer l'heure au format hh:mmXM (Exemple: 01:30PM). Vous n'avez pas correctement entré AM (signifie Ante Meridiem en latin) ou PM (signifie Post Meridiem en latin).")
+        exit()
 
 ### Error ###
 handle_argument_errors()
