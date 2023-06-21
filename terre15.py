91665a2ae41058e878b12d1213c0040934a9444f
elitalpa <elitalpa@gmail.com>
2023-06-21 23:19:26 +0200

    terre15.py - Earth : Okay git add terre15.py

---
+++ terre15.py
@@ -0,0 +1,21 @@
+##### Terre - Terre : ok #####
+# Créez un programme qui célèbre votre victoire.
+# Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+import random
+
+### Function ###
+def get_victory_message():
+    adjective_of_victory = ["tellurique", "gérable", "faisable"]
+    victory_index = random.randint(0,2)
+    return f"J’ai terminé l’Épreuve de la Terre et c’était {adjective_of_victory[victory_index]}."
+
+### Error ###
+
+### Parsing ###
+
+### Problem Solving ###
+victory_message = get_victory_message()
+
+### Result ###
+print(victory_message)
