3b193ca3763f6625eb6b7ae30af9656493908bbb
elitalpa <elitalpa@gmail.com>
2023-06-23 16:39:33 +0200

    terre02.py - separating the print() from the function and adding a function to handle argument errors

--- terre02.py
+++ terre02.py
@@ -4,16 +4,27 @@
 import sys
 
 ### Function ###
-def show_arguments_in_lines():
+def get_arguments_in_lines(arguments):
+    arguments_in_lines = ""
+
     for each_argument in arguments:
-        print(each_argument)
+        arguments_in_lines += f"{each_argument}\n"
+
+    return arguments_in_lines
+
+def handle_argument_errors():
+    if len(sys.argv) < 2:
+        print("erreur: Veuillez entrer un ou plusieurs arguments.")
+        exit()
 
 ### Error ###
+handle_argument_errors()
 
 ### Parsing ###
-arguments = sys.argv[1:]
+arguments_input = sys.argv[1:]
 
 ### Problem Solving ###
+arguments_in_lines_result = get_arguments_in_lines(arguments_input)
 
 ### Result ###
-show_arguments_in_lines()
+print(arguments_in_lines_result, end="")
